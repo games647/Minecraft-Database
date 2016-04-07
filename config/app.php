294d859e163ef1858b104a250ce8f894fa774533
games647 <games@tsprivat.de>
2016-04-07 18:36:16 +0200

    Added ide-helper

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //IDE-HELPER
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
